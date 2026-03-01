a264535534a7a7045df86a4ebe47029158d27976
anastasiia-kushch <anastasiia.kushch02@gmail.com>
2026-03-01 17:09:08 +0100

    finish hw

--- services/contactsServices.js
+++ services/contactsServices.js
@@ -1,61 +1,78 @@
 import Contact from '../models/contact.js';
 
-export async function listContacts() {
-  try {
-    const contacts = await Contact.findAll();
-    return contacts;
-  } catch (error) {
-    console.log(error);
-  }
+export async function listContacts(ownerId) {
+  const contacts = await Contact.findAll({
+    where: { owner: ownerId },
+  });
+
+  return contacts;
 }
 
-export async function getContactById(contactId) {
-  try {
-    const contact = await Contact.findByPk(contactId);
-    return contact;
-  } catch (error) {
-    console.log(error);
-  }
+export async function getContactById(contactId, ownerId) {
+  const contact = await Contact.findOne({
+    where: {
+      id: contactId,
+      owner: ownerId,
+    },
+  });
+
+  return contact;
 }
 
-export async function removeContact(contactId) {
-  try {
-    const contact = await Contact.findByPk(contactId);
-    if (!contact) return null;
-    await contact.destroy();
-    return contact;
-  } catch (error) {
-    console.log(error);
-  }
+export async function removeContact(contactId, ownerId) {
+  const contact = await Contact.findOne({
+    where: {
+      id: contactId,
+      owner: ownerId,
+    },
+  });
+
+  if (!contact) return null;
+
+  await contact.destroy();
+
+  return contact;
 }
 
-export async function addContact(name, email, phone) {
-  try {
-    const newContact = await Contact.create({ name, email, phone });
-    return newContact;
-  } catch (error) {
-    console.log(error);
-  }
+export async function addContact(name, email, phone, ownerId) {
+  const newContact = await Contact.create({
+    name,
+    email,
+    phone,
+    owner: ownerId,
+  });
+
+  return newContact;
 }
 
-export async function updateContact(contactId, data) {
-  try {
-    const contact = await Contact.findByPk(contactId);
-    if (!contact) return null;
-    const updated = await contact.update(data);
-    return updated;
-  } catch (error) {
-    console.log(error);
-  }
+export async function updateContact(contactId, data, ownerId) {
+  const contact = await Contact.findOne({
+    where: {
+      id: contactId,
+      owner: ownerId,
+    },
+  });
+
+  if (!contact) return null;
+
+  const updated = await contact.update(data);
+
+  return updated;
 }
 
-export async function updateStatusContact(contactId, body) {
-  try {
-    const contact = await Contact.findByPk(contactId);
-    if (!contact) return null;
-    const updated = await contact.update({ favorite: body.favorite });
-    return updated;
-  } catch (error) {
-    console.log(error);
-  }
+export async function updateStatusContact(contactId, body, ownerId) {
+  const contact = await Contact.findOne({
+    where: {
+      id: contactId,
+      owner: ownerId,
+    },
+  });
+
+  if (!contact) return null;
+
+  const updated = await contact.update({
+    favorite: body.favorite,
+  });
+
+  return updated;
 }
